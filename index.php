538d8a9d7c5158b7d27ad1dc657c6b087cc272ea
Stephan Broek <SABroek1@shef.ac.uk>
2015-01-04 23:37:17 +0100

    functionnally finished

--- index.php
+++ index.php
@@ -1,67 +1,46 @@
 <?php
 
-$url = substr($_SERVER['REQUEST_URI'],1) ?: 'index';
+define('DIR', dirname(__FILE__) . '/');
+define('TEMPLATE_DIR', DIR . 'templates/');
 
-?>
+require_once DIR . 'vendor/autoload.php';
+require_once DIR . 'fn.php';
 
-<!DOCTYPE html>
-<html>
-   <head>
+use \Smarty;
+use \Parsedown;
 
-      <title>Stephan</title>
+$smarty = new Smarty();
+$smarty->setTemplateDir(TEMPLATE_DIR);
+$smarty->setCompileDir(DIR . 'cache/templates/');
+$smarty->setConfigDir(DIR . 'config/');
+$smarty->setCacheDir(DIR . 'cache/');
 
-      <link href="css/normalize.css" rel="stylesheet" />
-      <link href="css/main.css" rel="stylesheet" />
 
-      <script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
+// Find the page required and default to index if none.
+$page = substr($_SERVER['REQUEST_URI'],1) ?: 'index';
 
-      <!--[if lt IE 9]>
-         <script src="js/html5shiv.js"></script>
-      <![endif]-->
-   </head>
-   <body>
-      <div id="outer">
-         <div id="header">
+if (!preg_match('/^[A-Za-z0-9_\/-]+$/', $page))
+   redirect('index');
 
-            <div id="heading">
-               Stephan Broek
-            </div>
+$smarty->assign('page', $page);
 
-            <nav>
-               <ul>
-                  <li><a <?php if ($url == 'index') echo ' class="active" '; ?> href="index">Home</a></li>
-                  <li><a <?php if ($url == 'me') echo ' class="active" '; ?> href="me">Me</a></li>
-                  <li><a <?php if ($url == 'projects') echo 'class="active" '; ?> href="projects">Projects</a></li>
-                  <li><a <?php if ($url == 'contact') echo 'class="active" '; ?> href="contact">Contact</a></li>
-                  <li><a class="external" href="https://github.com/stephanbroek">Github</a>
-                  <li><a class="external" href="https://twitter.com/stephanbroek">Twitter</a>
-            </nav>
-         </div>
-      </div>
+if (file_exists($file = DIR . "pages/$page.md"))
+{
+   $parsedown = new Parsedown();
+   $markdown = file_get_contents($file);
+   $html = $parsedown->text($markdown);
 
-      <div id="main">
+} 
+else if (file_exists($file = DIR . "pages/$page.html")) 
+{
+   $html = file_get_contents($file);
+}
+else
+{
+   redirect('index');
+}
 
-         LLL<?= $url;?>
-         
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla, nisl vel venenatis porttitor, erat erat vehicula nulla, a tincidunt nunc velit ut leo. Fusce mollis nibh vitae lectus aliquet tempus. Mauris gravida luctus accumsan. Donec euismod ornare ex, id pulvinar ipsum. Curabitur porttitor, arcu a ultrices mattis, est ante ultricies ligula, nec commodo lectus sem sed lectus. Proin cursus sapien quis tempor ornare. Nulla vitae tortor nec tortor viverra venenatis. Morbi accumsan blandit mi, vel imperdiet mi vestibulum vitae. Sed interdum odio sapien, quis fermentum ligula pellentesque sit amet. Morbi blandit lacus sagittis augue posuere tincidunt. Donec magna augue, faucibus ac tristique a, lobortis a metus. Etiam venenatis odio a lobortis convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam quis pulvinar lacus, nec pharetra dui. Aliquam finibus nulla urna, at volutpat ipsum tincidunt nec.</p>
+$smarty->assign('body', $html);
 
-         <p>Maecenas ac lectus tempus, gravida enim in, gravida felis. Curabitur ac erat id neque suscipit pulvinar. Cras a sollicitudin enim. Mauris sit amet tincidunt felis. Nulla facilisi. Nunc fermentum felis non justo tempor, id efficitur nisl vulputate. Proin vel laoreet massa, nec fringilla sapien. Morbi imperdiet, tortor non vehicula feugiat, sapien dui blandit lectus, in blandit metus augue vitae mi. Duis nunc dui, dapibus eget dolor in, semper lacinia ipsum. Duis ac tincidunt lectus. Morbi dignissim velit ut arcu ultricies, at commodo sem efficitur. Pellentesque ullamcorper justo libero, ac luctus orci vulputate sed. Nam sodales mollis arcu, ut fringilla leo viverra at. Vivamus pellentesque consectetur nulla quis semper. Curabitur aliquam sapien quis leo eleifend semper. In sit amet tempor nisi.</p>
+$smarty->display('main.tpl');
 
-         <p>Nunc erat nulla, dictum commodo interdum eu, hendrerit nec ligula. Morbi tempus felis felis, malesuada rutrum lacus porttitor vitae. Duis viverra euismod justo sit amet fermentum. Vivamus tempor diam ipsum, non pellentesque nisi aliquet a. In gravida iaculis metus, sit amet gravida neque vehicula eu. Vivamus feugiat accumsan dolor, ut finibus risus feugiat eu. Sed semper eros ante, a gravida urna pretium non. Nulla blandit lacus id sapien rutrum semper. Nunc aliquet posuere aliquam. Phasellus libero sapien, tincidunt at viverra eget, ultrices vitae orci. Donec vitae efficitur turpis. Proin nulla urna, vulputate eu erat in, porttitor porttitor dui. </p>
-
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla, nisl vel venenatis porttitor, erat erat vehicula nulla, a tincidunt nunc velit ut leo. Fusce mollis nibh vitae lectus aliquet tempus. Mauris gravida luctus accumsan. Donec euismod ornare ex, id pulvinar ipsum. Curabitur porttitor, arcu a ultrices mattis, est ante ultricies ligula, nec commodo lectus sem sed lectus. Proin cursus sapien quis tempor ornare. Nulla vitae tortor nec tortor viverra venenatis. Morbi accumsan blandit mi, vel imperdiet mi vestibulum vitae. Sed interdum odio sapien, quis fermentum ligula pellentesque sit amet. Morbi blandit lacus sagittis augue posuere tincidunt. Donec magna augue, faucibus ac tristique a, lobortis a metus. Etiam venenatis odio a lobortis convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam quis pulvinar lacus, nec pharetra dui. Aliquam finibus nulla urna, at volutpat ipsum tincidunt nec.</p>
-
-         <p>Maecenas ac lectus tempus, gravida enim in, gravida felis. Curabitur ac erat id neque suscipit pulvinar. Cras a sollicitudin enim. Mauris sit amet tincidunt felis. Nulla facilisi. Nunc fermentum felis non justo tempor, id efficitur nisl vulputate. Proin vel laoreet massa, nec fringilla sapien. Morbi imperdiet, tortor non vehicula feugiat, sapien dui blandit lectus, in blandit metus augue vitae mi. Duis nunc dui, dapibus eget dolor in, semper lacinia ipsum. Duis ac tincidunt lectus. Morbi dignissim velit ut arcu ultricies, at commodo sem efficitur. Pellentesque ullamcorper justo libero, ac luctus orci vulputate sed. Nam sodales mollis arcu, ut fringilla leo viverra at. Vivamus pellentesque consectetur nulla quis semper. Curabitur aliquam sapien quis leo eleifend semper. In sit amet tempor nisi.</p>
-
-         <p>Nunc erat nulla, dictum commodo interdum eu, hendrerit nec ligula. Morbi tempus felis felis, malesuada rutrum lacus porttitor vitae. Duis viverra euismod justo sit amet fermentum. Vivamus tempor diam ipsum, non pellentesque nisi aliquet a. In gravida iaculis metus, sit amet gravida neque vehicula eu. Vivamus feugiat accumsan dolor, ut finibus risus feugiat eu. Sed semper eros ante, a gravida urna pretium non. Nulla blandit lacus id sapien rutrum semper. Nunc aliquet posuere aliquam. Phasellus libero sapien, tincidunt at viverra eget, ultrices vitae orci. Donec vitae efficitur turpis. Proin nulla urna, vulputate eu erat in, porttitor porttitor dui. </p>
-
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla, nisl vel venenatis porttitor, erat erat vehicula nulla, a tincidunt nunc velit ut leo. Fusce mollis nibh vitae lectus aliquet tempus. Mauris gravida luctus accumsan. Donec euismod ornare ex, id pulvinar ipsum. Curabitur porttitor, arcu a ultrices mattis, est ante ultricies ligula, nec commodo lectus sem sed lectus. Proin cursus sapien quis tempor ornare. Nulla vitae tortor nec tortor viverra venenatis. Morbi accumsan blandit mi, vel imperdiet mi vestibulum vitae. Sed interdum odio sapien, quis fermentum ligula pellentesque sit amet. Morbi blandit lacus sagittis augue posuere tincidunt. Donec magna augue, faucibus ac tristique a, lobortis a metus. Etiam venenatis odio a lobortis convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam quis pulvinar lacus, nec pharetra dui. Aliquam finibus nulla urna, at volutpat ipsum tincidunt nec.</p>
-
-         <p>Maecenas ac lectus tempus, gravida enim in, gravida felis. Curabitur ac erat id neque suscipit pulvinar. Cras a sollicitudin enim. Mauris sit amet tincidunt felis. Nulla facilisi. Nunc fermentum felis non justo tempor, id efficitur nisl vulputate. Proin vel laoreet massa, nec fringilla sapien. Morbi imperdiet, tortor non vehicula feugiat, sapien dui blandit lectus, in blandit metus augue vitae mi. Duis nunc dui, dapibus eget dolor in, semper lacinia ipsum. Duis ac tincidunt lectus. Morbi dignissim velit ut arcu ultricies, at commodo sem efficitur. Pellentesque ullamcorper justo libero, ac luctus orci vulputate sed. Nam sodales mollis arcu, ut fringilla leo viverra at. Vivamus pellentesque consectetur nulla quis semper. Curabitur aliquam sapien quis leo eleifend semper. In sit amet tempor nisi.</p>
-
-         <p>Nunc erat nulla, dictum commodo interdum eu, hendrerit nec ligula. Morbi tempus felis felis, malesuada rutrum lacus porttitor vitae. Duis viverra euismod justo sit amet fermentum. Vivamus tempor diam ipsum, non pellentesque nisi aliquet a. In gravida iaculis metus, sit amet gravida neque vehicula eu. Vivamus feugiat accumsan dolor, ut finibus risus feugiat eu. Sed semper eros ante, a gravida urna pretium non. Nulla blandit lacus id sapien rutrum semper. Nunc aliquet posuere aliquam. Phasellus libero sapien, tincidunt at viverra eget, ultrices vitae orci. Donec vitae efficitur turpis. Proin nulla urna, vulputate eu erat in, porttitor porttitor dui. </p>
-
-      </div>
-
-   </body>
-</html>
---
+++ templates/main.tpl
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+   <head>
+
+      <title>Stephan</title>
+
+      <link href="css/normalize.css" rel="stylesheet" />
+      <link href="css/main.css" rel="stylesheet" />
+
+      <script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
+
+      <!--[if lt IE 9]>
+         <script src="js/html5shiv.js"></script>
+      <![endif]-->
+   </head>
+   <body>
+      <div id="outer">
+         <div id="header">
+
+            <div id="heading">
+               Stephan Broek
+            </div>
+
+            <nav>
+               <ul>
+                  <li><a {if $page == 'index'}class="active"{/if} href="index">Home</a></li>
+                  <li><a {if $page == 'me'}class="active"{/if} href="me">Me</a></li>
+                  <li><a {if $page == 'projects'}class="active"{/if} href="projects">Projects</a></li>
+                  <li><a {if $page == 'contact'}class="active"{/if} href="contact">Contact</a></li>
+                  <li><a class="external" href="https://github.com/stephanbroek">Github</a>
+                  <li><a class="external" href="https://twitter.com/stephanbroek">Twitter</a>
+            </nav>
+         </div>
+      </div>
+
+      <div id="main">
+
+         {$body}
+
+      </div>
+
+   </body>
+</html>
